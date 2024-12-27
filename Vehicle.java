c51249d9ada54877a3f9aed830aaa82d7d260cdc
Adwoa-p <pokuaaa158@gmail.com>
2024-12-27 17:49:18 +0000

    created classes

---
+++ Vehicle.java
@@ -0,0 +1,52 @@
+public abstract class Vehicle {
+    private String vehicleId;
+    private String model;
+    private double baseRentalRate;
+    private boolean isAvailable;
+
+    public Vehicle(String vehicleId, String model, double baseRentalRate, boolean isAvailable) {
+        this.vehicleId = vehicleId;
+        this.model = model;
+        this.baseRentalRate = baseRentalRate;
+        this.isAvailable = isAvailable;
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(String vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getBaseRentalRate() {
+        return baseRentalRate;
+    }
+
+    public void setBaseRentalRate(double baseRentalRate) {
+        this.baseRentalRate = baseRentalRate;
+    }
+
+    public boolean isAvailable() {
+        return this.isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public abstract boolean isAvailableForRental();
+
+    public abstract double calculateRentalCost(int days);
+
+}
+
+
